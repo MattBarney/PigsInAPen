aeece73579a9b4060c485606a5d3b12353f52469
jwmatson <jwmatson@ualberta.ca>
2019-10-27 21:42:25 -0600

    The basic prototype for Pigs in a Pen

---
+++ app/src/main/java/com/example/pigsinapen/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.pigsinapen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void showPopup(View v){
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.popup_menu);
+        popup.show();
+
+    }//showPopup
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+
+        switch (item.getItemId()) {
+             default:
+                 Intent goToBoard = new Intent(getApplicationContext(), GameBoard.class);
+                 this.startActivity(goToBoard);
+                 return true;
+
+        }//switch
+
+    }//onMenuItemClick
+
+}
