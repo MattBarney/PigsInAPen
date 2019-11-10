71760068a9bfcad4f7af93e01801871e33b1439c
jwmatson <matsonj2013@gmail.com>
2019-11-10 13:49:28 -0700

    Added proper documentation to the fences class. Also added an extra button to MainActivity

--- app/src/main/java/com/example/pigsinapen/MainActivity.java
+++ app/src/main/java/com/example/pigsinapen/MainActivity.java
@@ -5,50 +5,38 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Layout;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.PopupMenu;
-import android.widget.RelativeLayout;
 
-
-public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    Fences wack = new Fences(1,1,false,MainActivity.this);
-    Fences wack2 = new Fences(1,1,false,MainActivity.this);
-    Fences wack3 = new Fences(1,1,false,MainActivity.this);
-    ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.activity_main);
+    Fences wack = new Fences(1, 1, false, MainActivity.this);
+    Fences wack2 = new Fences(1, 1, false, MainActivity.this);
+    Fences wack3 = new Fences(1, 1, true, MainActivity.this);
+    ConstraintLayout layout = findViewById(R.id.activity_main);
 
+    wack.getButton().setX(50);
+    wack.getButton().setY(75);
+
+    wack2.getButton().setX(215);
+    wack2.getButton().setY(75);
+    wack3.getButton().setX(130);
+    wack3.getButton().setY(155);
     layout.addView(wack.getButton());
     layout.addView(wack2.getButton());
     layout.addView(wack3.getButton());
-
   }
 
-  public void showPopup(View v) {
-    PopupMenu popup = new PopupMenu(this, v);
-    popup.setOnMenuItemClickListener(this);
-    popup.inflate(R.menu.popup_menu);
-    popup.show();
+  public void goToSettings(View v) {
+    Intent goToSettings = new Intent(getApplicationContext(), Settings.class);
+    startActivity(goToSettings);
+  } // goToSettings
 
-  } // showPopup
-
-  @Override
-  public boolean onMenuItemClick(MenuItem item) {
-
-    switch (item.getItemId()) {
-      default:
-        Intent goToBoard = new Intent(getApplicationContext(), Settings.class);
-        this.startActivity(goToBoard);
-        return true;
-    } // switch
-  } // onMenuItemClick
-
-
+  public void goToGameDisplay(View v) {
+    Intent goToGameDisplay = new Intent(getApplicationContext(), GameDisplay.class);
+    startActivity(goToGameDisplay);
+  } // goToGameDisplay
 }
