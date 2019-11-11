0e55d6ad3397dc7f4872fc2bab3762e6720a2d02
MattBarney <barney@ualberta.ca>
2019-11-11 00:28:39 -0700

    Rewrote toggle method, added method for changing grid sizes

--- app/src/main/java/com/example/pigsinapen/Settings.java
+++ app/src/main/java/com/example/pigsinapen/Settings.java
@@ -2,13 +2,11 @@
  * Settings.java
  *
  * Activity for changing the type of game, names of the players, and size of the game board.
- * <p>
- * File and UI originally created by Jared Matson.
- * Instance variables, methods, and UI updates done by Matt Barney.
- * <p>
+ *
+ * File and UI originally created by Jared Matson. Instance variables, methods, and UI updates
+ * done by Matt Barney.
+ *
  * Methods
- * -
- * -
  */
 package com.example.pigsinapen;
 
@@ -17,9 +15,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import com.example.pigsinapen.R;
+
+import org.w3c.dom.Text;
 
 public class Settings extends AppCompatActivity {
 
@@ -33,51 +35,111 @@ public class Settings extends AppCompatActivity {
 
   private int height;
 
-  //Collection of grid sizes the player can choose from
+  // Collection of grid sizes the player can choose from
   private final String[] gridSizes = {"3x3", "4x3", "5x3", "4x4", "5x4"};
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_settings);
+
+    TextView gridSize = findViewById(R.id.gridSizeText);
+
+    gridSize.setText(gridSizes[2]);
   }
 
+  // BUTTON METHODS \\
+
   /**
-   * Sets game to versus human.
+   * Switches the opponent to human or computer.
    *
-   * When toggled this sets the game to versus a human, displays the player two name field and
-   * toggles the versus computer option off.
+   * <p>Checks which toggle button was pressed. If the human toggle button was pressed, toggle the
+   * computer button off, make the player two name field visible, and sets the AI toggle to false.
    *
-   * @param humanToggleButton Toggle button that activates this method.
+   * <p>If the computer toggle button is pressed, toggle the human button off, make the player two
+   * name field invisible, and set the AI toggle to true.
+   *
+   * @param buttonPressed - Toggle button that called this method, in this case either humanToggle
+   *     or computerToggle.
    */
-  public void toggleHumanOpponent(View humanToggleButton) {
+  public void toggleOpponent(View buttonPressed) {
     ToggleButton toggleHuman = findViewById(R.id.humanToggle);
     ToggleButton toggleComputer = findViewById(R.id.computerToggle);
     EditText playerTwoName = findViewById(R.id.enterPlayerTwoName);
 
-    toggleHuman.setChecked(true);
-    toggleComputer.setChecked(false);
-    playerTwoName.setVisibility(View.VISIBLE);
-    aiToggle = false;
+    if (buttonPressed == toggleHuman) {
+      toggleHuman.setChecked(true);
+      toggleComputer.setChecked(false);
+      playerTwoName.setVisibility(View.VISIBLE);
+      aiToggle = false;
+    } else { // buttonPressed == toggleComputer
+      toggleHuman.setChecked(false);
+      toggleComputer.setChecked(true);
+      playerTwoName.setVisibility(View.INVISIBLE);
+      aiToggle = true;
+    }
   }
 
   /**
-   * Sets game to versus computer.
+   * Increases or decreases the chosen grid size
    *
-   * When toggled this sets the game to versus a computer, hides the player two name field and
-   * toggles the versus human option off.
+   * <p>Increases the grid size if the increaseGrid button was pressed, decreases the grid size if
+   * the decreaseGrid button was pressed. If the first or last size option is currently being
+   * displayed, hide the respective button to avoid exceptions.
    *
-   * @param computerToggleButton Toggle button that activates this method.
+   * @param buttonPressed - The button that called this method, in this case either increaseGrid or
+   *     decreaseGrid.
    */
-  public void toggleComputerOpponent(View computerToggleButton) {
-    ToggleButton toggleComputer = findViewById(R.id.computerToggle);
-    ToggleButton toggleHuman = findViewById(R.id.humanToggle);
-    EditText playerTwoName = findViewById(R.id.enterPlayerTwoName);
+  public void changeGridSize(View buttonPressed) {
+    ImageButton increaseSize = findViewById(R.id.increaseGrid);
+    ImageButton decreaseSize = findViewById(R.id.decreaseGrid);
+    TextView gridSize = findViewById(R.id.gridSizeText);
 
-    toggleComputer.setChecked(true);
-    toggleHuman.setChecked(false);
-    playerTwoName.setVisibility(View.INVISIBLE);
-    aiToggle = true;
+    int currentSizeIndex = getCurrentSizeIndex();
+
+    if (buttonPressed == increaseSize) {
+      gridSize.setText(gridSizes[currentSizeIndex + 1]);
+
+      // The decrease button becomes invisible if it would cause an exception
+      // if it were pressed another time, increasing the index gets rid of that risk,
+      // so make the button visible again.
+      decreaseSize.setVisibility(View.VISIBLE);
+
+      // If increasing the index one more time would go out of bounds, hide the button.
+      if (currentSizeIndex + 2 >= gridSizes.length) {
+        increaseSize.setVisibility(View.INVISIBLE);
+      }
+
+    } else { // buttonPressed == decreaseSize
+      gridSize.setText(gridSizes[currentSizeIndex - 1]);
+
+      // Same idea as above.
+      increaseSize.setVisibility(View.VISIBLE);
+
+      // If decreasing the index one more time would go out of bounds, hide the button.
+      if (currentSizeIndex - 2 < 0) {
+        decreaseSize.setVisibility(View.INVISIBLE);
+      }
+    }
   }
 
+  // HELPER METHODS \\
+
+  /** Gets the location of the current grid size in gridSizes[] */
+  private int getCurrentSizeIndex() {
+    TextView gridSize = findViewById(R.id.gridSizeText);
+
+    int sizeIndex = 0;
+
+    // The TextView's contents are set in onCreate from gridSizes[], since these contents are
+    // never changed to something not in gridSizes[] we don't need to worry about the size not
+    // being found.
+    for (int i = 0; i < gridSizes.length; i++) {
+      if (gridSize.getText() == gridSizes[i]) {
+        sizeIndex = i;
+      }
+    }
+
+    return sizeIndex;
+  }
 }
