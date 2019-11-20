6756195608290c756bda4f88327fce537705f0a4
MattBarney <barney@ualberta.ca>
2019-11-20 15:55:06 -0700

    Started checkFences function.

--- app/src/main/java/com/example/pigsinapen/ComputerPlayer.java
+++ app/src/main/java/com/example/pigsinapen/ComputerPlayer.java
@@ -6,4 +6,26 @@ public class ComputerPlayer extends Player {
     super(name, currentPlayer);
   }//constructor
 
+  /**
+   *
+   * @return
+   */
+  private Boolean checkFences(Integer width, Integer height, Boolean horizontal) {
+    Integer boxesClosed;
+
+    for (int i = 0; i < width; i++) {
+      for (int j = 0; j < height; j++) {
+        boxesClosed = board.checkBoxes(i, j, true);
+        if (boxesClosed > 0) {
+          //Set the button chosen to the 2nd player colour
+          //Make the button un-clickable
+          this.addToScore(boxesClosed);
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
 }
