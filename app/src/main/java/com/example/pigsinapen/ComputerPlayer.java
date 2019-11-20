431088f78d5cc6b4b43b63352f9df5a5ee2535d9
MattBarney <barney@ualberta.ca>
2019-11-20 15:40:10 -0700

    Added ComputerPlayer class and constructor.

---
+++ app/src/main/java/com/example/pigsinapen/ComputerPlayer.java
@@ -0,0 +1,9 @@
+package com.example.pigsinapen;
+
+public class ComputerPlayer extends Player {
+
+  ComputerPlayer(String name, boolean currentPlayer) {
+    super(name, currentPlayer);
+  }//constructor
+
+}
