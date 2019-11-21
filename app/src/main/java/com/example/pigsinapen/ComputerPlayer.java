b6c0c79697730310a41ccde509fa3a1c066693af
MattBarney <barney@ualberta.ca>
2019-11-20 18:10:27 -0700

    Finished checkFences function.

--- app/src/main/java/com/example/pigsinapen/ComputerPlayer.java
+++ app/src/main/java/com/example/pigsinapen/ComputerPlayer.java
@@ -4,21 +4,31 @@ public class ComputerPlayer extends Player {
 
   ComputerPlayer(String name, boolean currentPlayer) {
     super(name, currentPlayer);
-  }//constructor
+  } // constructor
 
   /**
+   * Looks for a fence that would result in a score.
    *
-   * @return
+   * <p>Goes through the the specified fence array and checks to see if picking a fence would result
+   * in gaining a point. If a fence results in a score that fence will be made unclickable and have
+   * its colour changed to this player's colour.
+   *
+   * @param width The number of rows present in the specified fence array.
+   * @param height The number of columns present in the specified fence array.
+   * @param horizontal Specifies which fence array to check. If this value is true check the
+   *     horizontal fence array, otherwise check the vertical fence array.
+   * @param board The game board currently being played on.
+   * @return True if a fence results in a score, false otherwise.
    */
-  private Boolean checkFences(Integer width, Integer height, Boolean horizontal) {
+  private Boolean checkFences(Integer width, Integer height, Boolean horizontal, GameBoard board) {
     Integer boxesClosed;
 
     for (int i = 0; i < width; i++) {
       for (int j = 0; j < height; j++) {
         boxesClosed = board.checkBoxes(i, j, true);
         if (boxesClosed > 0) {
-          //Set the button chosen to the 2nd player colour
-          //Make the button un-clickable
+          // Set the button chosen to the 2nd player colour
+          // Make the button un-clickable
           this.addToScore(boxesClosed);
           return true;
         }
@@ -27,5 +37,4 @@ public class ComputerPlayer extends Player {
 
     return false;
   }
-
 }
