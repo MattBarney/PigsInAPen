3f54e299129da5f6c1e716e601967cb3741fe4cc
jwmatson <matsonj2013@gmail.com>
2019-11-21 16:04:19 -0700

    Altered the buttons in the GameDisplay so that they are centered (only for 6x6 right now)

--- app/src/main/java/com/example/pigsinapen/GameDisplay.java
+++ app/src/main/java/com/example/pigsinapen/GameDisplay.java
@@ -2,6 +2,7 @@ package com.example.pigsinapen;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,35 +13,21 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class GameDisplay extends AppCompatActivity implements View.OnClickListener {
-
   LinearLayout game_board_layout;
-
   Player player1, player2;
   GameBoard gameBoard;
   Fences fence;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_game_display);
-    gameBoard = new GameBoard(6,6,GameDisplay.this, this);
+    gameBoard = new GameBoard(6, 6, GameDisplay.this, this);
     createHorizontalFencesandDots();
     createVerticalFences();
-
-
     player1 = new Player("Alvee", 3, true);
     player2 = new Player("Jared", 2, false);
 
-
-    //wack = new Fences(1,2,true,GameDisplay.this,GameDisplay.this);
-//    Button[][] buttons = new Button[4][4];
-//    for (int i = 0; i < 4; i++) {
-//      for (int j = 0; j < 4; j++) {
-//        String buttonID = "button_" + i + j;
-//        int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
-//        buttons[i][j] = findViewById(resID);
-//        buttons[i][j].setOnClickListener(this);
-//      }
-//    }
   }
 
   //  back button
@@ -71,6 +58,7 @@ public class GameDisplay extends AppCompatActivity implements View.OnClickListen
   // }
   // ------------game display----------------
   public void playerTurn(int row, int col, boolean horizontal) {
+    System.out.println(row + " " + col + " " + horizontal);
     Player currentPlayer = getCurrentPlayer();
     Player otherPlayer = getOtherPlayer();
     Integer closedBoxes = gameBoard.checkBoxes(row, col, horizontal);
@@ -99,41 +87,37 @@ public class GameDisplay extends AppCompatActivity implements View.OnClickListen
   void displayWinner() {
     if (player1.getScore() > player2.getScore())
       Toast.makeText(GameDisplay.this, player1.getName() + "wins", Toast.LENGTH_SHORT).show();
-    else
-      Toast.makeText(GameDisplay.this, player2.getName() + "wins", Toast.LENGTH_SHORT).show();
-    //displayWinner
+    else Toast.makeText(GameDisplay.this, player2.getName() + "wins", Toast.LENGTH_SHORT).show();
+    // displayWinner
   }
 
   // ------CALL QUIT and REPLAY--------
-  void quit(){
+  void quit() {}
 
-  }
+  void replay() {}
 
-  void replay(){
+  void createHorizontalFencesandDots() {
 
-  }
-  void createHorizontalFencesandDots(){
     int putFenceX = 0;
-    int putFenceY = 333;
+    int putFenceY = 0;
     int putButtonX = 0;
-    int putButtonY = 375;
+    int putButtonY = 44;
 
-    for (int j = 0; j < 6; j += 1) {
-      putFenceX = 200;
-      putFenceY += 175;
-      putButtonX = 105;
-      putButtonY += 175;
-      for (int i = 0; i < 5; i += 1) {
-        ConstraintLayout layout = findViewById(R.id.activity_game_display);
+    for (int j = 0; j < 6; j += 1) { //total amount of rows
+      putFenceX = 95;// + 150;
+      putFenceY += 183;
+      putButtonX = 0;// + 150;
+      putButtonY += 183;
+      for (int i = 0; i < 5; i += 1) { //num of hor lines per row
+        ConstraintLayout layout = findViewById(R.id.boardGameConstraint);
         Fences fence = new Fences(j, i, true, GameDisplay.this, this);
         fence.getButton().setX(putFenceX);
         fence.getButton().setY(putFenceY);
         layout.addView(fence.getButton());
         putFenceX += 170;
-
       } // forHorfences
-      for(int i = 0; i < 6; i += 1){
-        ConstraintLayout layout = findViewById(R.id.activity_game_display);
+      for (int i = 0; i < 6; i += 1) {//dots per row
+        ConstraintLayout layout = findViewById(R.id.boardGameConstraint);
         ImageView dot = new ImageView(this);
         dot.setX(putButtonX);
         dot.setY(putButtonY);
@@ -144,26 +128,25 @@ public class GameDisplay extends AppCompatActivity implements View.OnClickListen
         layoutParams.height = 35;
         putButtonX += 170;
         dot.setLayoutParams(layoutParams);
-      }//fordot
-    }//outerfor
+      } // fordot
+    } // outerfor
   }
-  void createVerticalFences(){
-    int putFenceXVert = 0;
-    int putFenceYVert = 415;
 
-    for(int j = 0; j < 5; j += 1){
-      putFenceXVert = 113;
-      putFenceYVert += 175;
-      for (int i = 0; i < 6; i += 1) {
-        ConstraintLayout layout = findViewById(R.id.activity_game_display);
+  void createVerticalFences() {
+    int putFenceXVert = 0;
+    int putFenceYVert = 89;
+
+    for (int j = 0; j < 5; j += 1) { //num rows
+      putFenceXVert = 10; //+ 150;
+      putFenceYVert += 183;
+      for (int i = 0; i < 6; i += 1) { //lines per row
+        ConstraintLayout layout = findViewById(R.id.boardGameConstraint);
         Fences fence = new Fences(j, i, false, GameDisplay.this, this);
         fence.getButton().setX(putFenceXVert);
         fence.getButton().setY(putFenceYVert);
         layout.addView(fence.getButton());
         putFenceXVert += 170;
-
       } // forfences
-    }//for
+    } // for
   }
-
 }
