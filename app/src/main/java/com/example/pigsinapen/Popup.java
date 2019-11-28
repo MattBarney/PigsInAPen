c180ff8ca4110b45377759e6481cbbe82f7e110d
alvee2020 <alvee@ualberta.ca>
2019-11-28 13:23:03 -0700

    Popup class, commented, name fixed, etc

--- app/src/main/java/com/example/pigsinapen/Popup.java
+++ app/src/main/java/com/example/pigsinapen/Popup.java
@@ -1,3 +1,11 @@
+/**
+ * Popup.java
+ *
+ * This class shows the pop up window in the screen whoever wins the game, could also be tied game.
+ *
+ * @version 1.0
+ * @since 2019-11-27
+ */
 package com.example.pigsinapen;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -14,43 +22,46 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class Popup extends Activity {
-
-  Button gobackMain;
+  /**
+   * goBackMainMenu button is to go back to main menu
+   */
+  Button goBackMain;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_popup);
 
+    // whoWon TextView is to show the winner's name
     TextView whoWon = findViewById(R.id.whoWon);
     String name = getIntent().getStringExtra("player_name");
     name = name + " Wins !";
     whoWon.setText(name);
 
-    gobackMain = (Button) findViewById(R.id.gobackMain);
-    gobackMain.setOnClickListener(new View.OnClickListener() {
+    // goBackMain button is getting its button id from XML code.
+    goBackMain = (Button) findViewById(R.id.gobackMain);
+    goBackMain.setOnClickListener(new View.OnClickListener() {
+
       @Override
       public void onClick(View view) {
         Intent i = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(i);
-
+        finish();
       }
     });
 
-    ConstraintLayout layout = findViewById(R.id.layout);
+    ConstraintLayout layoutPopupWindow = findViewById(R.id.layoutPopupWindow);
     DisplayMetrics dm = new DisplayMetrics();
     getWindowManager().getDefaultDisplay().getMetrics(dm);
 
     float width = dm.widthPixels;
     float height = dm.heightPixels;
 
+    // sets the popup window in the middle of the screen and a size defined by half of height and 1/10th of width
     getWindow().setLayout((int)(width * .9), (int)(height * .5));
 
-
     WindowManager.LayoutParams params = getWindow().getAttributes();
-
     params.gravity = Gravity.CENTER;
-
 
     getWindow().setAttributes(params);
 
