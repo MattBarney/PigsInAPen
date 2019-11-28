8ad3fa4d1aa133bb29b1b642a6b0eefd8f5b9a57
jwmatson <matsonj2013@gmail.com>
2019-11-27 18:52:15 -0700

    Added setHorFenceY function to GameDisplay.

--- app/src/main/java/com/example/pigsinapen/GameDisplay.java
+++ app/src/main/java/com/example/pigsinapen/GameDisplay.java
@@ -95,7 +95,7 @@ public class GameDisplay extends AppCompatActivity {
   }
 
   void sizeOfGridToMake(int size){
-    int putHorFenceX = setHorFenceX(size);
+    int putHorFenceX = setHorFenceX(size, size);
     int putHorFenceY = 0;
     int putButtonX = 0;
     int putButtonY = 0;
@@ -104,7 +104,7 @@ public class GameDisplay extends AppCompatActivity {
 
     switch (size){
       case 4:
-        putHorFenceY = 100;
+        //putHorFenceY = 100;
         putButtonY = 144;
         putVertFenceY = 189 ;
         //putHorFenceX = 250;
@@ -112,7 +112,7 @@ public class GameDisplay extends AppCompatActivity {
         putVertFenceX = 166;
         break;
       case 5:
-        putHorFenceY = 0;
+       // putHorFenceY = 0;
         putButtonY = 44;
         putVertFenceY = 89;
         //putHorFenceX = 170;
@@ -120,7 +120,7 @@ public class GameDisplay extends AppCompatActivity {
         putVertFenceX = 86;
         break;
       case 6:
-        putHorFenceY = 0;
+       // putHorFenceY = 0;
         putButtonY = 44;
         putVertFenceY = 89;
         //putHorFenceX = 90;
@@ -132,15 +132,29 @@ public class GameDisplay extends AppCompatActivity {
       createHorizontalFencesAndDots(putHorFenceX,putHorFenceY,putButtonX,putButtonY,size,size);
       createVerticalFences(putVertFenceX,putVertFenceY, size, size);
   }//sizeOfGridToMake
-  int setHorFenceX(int size){
-    switch (size) {
-      case 4:
-        return 250;
-      case 5:
-        return 170;
-      case 6:
-        return 90;
-    }//switch
+  int setHorFenceX(int rowSize,int colSize){
+    if(rowSize == 4 && colSize == 4){
+      return 100;
+    }//else if
+    else if(rowSize == 5 && colSize == 5){
+      return 170;
+    }//else if
+    else if(rowSize == 6 && colSize == 6){
+      return 90;
+    }//else if
+    return 0;
+  }//setHorFenceX
+
+  int setHorFenceY(int rowSize,int colSize){
+    if(rowSize == 4 && colSize == 4){
+      return 250;
+    }//else if
+    else if(rowSize == 5 && colSize == 5){
+      return 89;
+    }//else if
+    else if(rowSize == 6 && colSize == 6){
+      return 0;
+    }//else if
     return 0;
   }//setHorFenceX
   /**
